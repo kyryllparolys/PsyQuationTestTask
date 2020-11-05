2268c7f59bbb1f2fb1a32a0b909754abdef710ab
Kyryll <kyryllpar@gmail.com>
2020-11-05 21:29:34 +0200

    Light refactoring.

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from accounts.models import Account
 from accounts.serializers import AccountSerializer
+
 
 class AccountsViewSet(viewsets.ModelViewSet):
     queryset = Account.objects.all()
