780a0602ba85df4604dd8c2999bbab19b80f143c
Kyryll <kyryllpar@gmail.com>
2020-11-05 23:33:55 +0200

    Started working on filters, finished is_new and is_active filters.

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Account(models.Model):
     name = models.CharField(max_length=50)
-    opened_at = models.DateTimeField(auto_now=True)
+    opened_at = models.DateTimeField(auto_now=True)  # TODO: test with no "auto_now"
 
     def __str__(self):
         return f'({self.id}) Account: {self.name}'
