4fb42491733afa2b74b942c35a80977fc2b2b0d0
Kyryll <kyryllpar@gmail.com>
2020-11-05 21:27:29 +0200

    Added serializers and viewsets for both accounts and transactions.

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+from accounts import views
+
+router = SimpleRouter()
+router.register(r'accounts', views.AccountsViewSet, basename='accounts')
+
+urlpatterns = router.urls
